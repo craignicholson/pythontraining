c76d6d4310b9d0a297d5007ad1b17f763cc9ca7a
Craig Nicholson <cnicholson@electsolve.com>
2018-05-05 10:17:27 -0400

    first commit

---
+++ words.py
@@ -0,0 +1,17 @@
+from urllib.request import urlopen
+with urlopen('http://sixty-north.com/c/t.txt') as story:
+    story_words_byte =[]
+    story_words = []
+
+    for line in story:
+        line_words_byte = line.split()
+        line_words = line.decode('UTF-8').split()
+
+        for word in line_words_byte:
+            story_words_byte.append(word)
+
+        for word in line_words:
+            story_words.append(word)
+
+print(story_words)
+print(story_words_byte)
