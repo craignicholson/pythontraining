9b727147f3616d340910ccb0071142dbfcc6f095
Craig Nicholson <cnicholson@electsolve.com>
2018-05-06 09:58:21 -0400

    Made a module from the original words.py example

--- pyfund/words.py
+++ pyfund/words.py
@@ -1,20 +1,78 @@
+#!/usr/bin/env python3
+"""Retrieve and print words from a URL.
+
+Usage:
+    python3 words.py <URL>
+
+"""
+import sys
 from urllib.request import urlopen
-with urlopen('http://sixty-north.com/c/t.txt') as story:
-    story_words_byte =[]
-    story_words = []
 
-    for line in story:
-        line_words_byte = line.split()
-        line_words = line.decode('UTF-8').split()
+# #! on mac or linux we must mark the .py file as executable
+# helps users see the correct version of python to use with this .py
+# chmod +x words.py
+# now we can run: $> ./words.py http://sixty-north.com/c/t.txt
+# pylauncher will help run the run env for your code
 
-        for word in line_words_byte:
-            story_words_byte.append(word)
+# Creating a function example
+# Open REPL type: import words
+# type words.fetch_words()
+# we can only call this within the py REPL
+def fetch_words(url):
+    """Fetch a list of words from a URL.
 
-        for word in line_words:
-            story_words.append(word)
+    Args:
+        url: The URL of a UTF-8 text document
 
-for word in story_words:
-    print(word)
+    Returns:
+        A list of strings containing the words
+        from a document.
+    """
+    with urlopen(url) as story:
+        story_words_byte = []
+        story_words = []
 
-#print(story_words)
-#print(story_words_byte)
+        for line in story:
+            line_words_byte = line.split()
+            line_words = line.decode('UTF-8').split()
+
+            for word in line_words_byte:
+                story_words_byte.append(word)
+
+            for word in line_words:
+                story_words.append(word)
+
+    return story_words
+
+
+def print_items(items):
+    """Print items one per line.
+
+    Args:
+        An iterable series of printable items.S
+    """
+    for item in items:
+        print(item)
+
+
+def main(url):
+    """Print each word from a text document from a URL
+
+    Args:
+        url: The URL of a UTF-8 text document.
+    """
+    words = fetch_words(url)
+    print_items(words)
+
+
+# To make this a module we can call form the system prompt
+# we need to have the double underscores with the name
+# __main__ as an example
+# this print(__name__) module level code is executed only once on first import
+# this will print __main__
+# to get this to execute out function we will add in if statement
+if __name__ == '__main__':
+    main(sys.argv[1]) # The 0th arg is the module filename
+
+# print(story_words)
+# print(story_words_byte)
